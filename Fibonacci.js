fee59ca2b15c9512138c64cb76d9b0309a0bd448
Cássio <cassiocr2012@hotmail.com>
2023-08-24 10:40:49 -0300

    new file:   Fibonacci.js

---
+++ Fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(n){
+    if((n == 1)||(n == 2)){
+        return 1
+    }
+    else{
+        return (fibonacci(n-1) + fibonacci(n-2))
+    }
+}
+
+var result = fibonacci(7)
+console.log(result)
