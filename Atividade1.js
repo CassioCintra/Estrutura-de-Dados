400e6db637709c072b1bd48f963c698672dc4927
Cássio <cassiocr2012@hotmail.com>
2023-08-31 09:36:18 -0300

    modified:   Atividade1.js

--- Atividade1.js
+++ Atividade1.js
@@ -1,83 +1,86 @@
-// Implementação dos algoritmos de ordenação
-
-function bubblesort(arr) {
-    const n = arr.length;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+function bubbleSort(vetor){
+    for (let i = 0; i < vetor.length; i++) {
+        for (let j = 0; j < vetor.length - 1 - i; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                let auxiliar = vetor[j]
+                vetor[j] = vetor[j + 1]
+                vetor[j + 1] = auxiliar
             }
         }
     }
-}
 
-function selectionSort(arr) {
-    const n = arr.length;
-    for (let i = 0; i < n; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+}
+function selectionSort(vetor){
+    for (i = 0; i < vetor.length; i++){
+        var small = vetor[i]
+        var position = i
+        for(j = i+1; j < vetor.length; j++){
+            if(small > vetor[j]){
+                small = vetor[j]
+                position = j
             }
         }
-        if (minIndex !== i) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        var assistant = vetor[i]
+        vetor [i] = vetor [position]
+        vetor[position] = assistant
+    }
+}
+function insertionSort(vetor){
+    for (i = 1; i < vetor.length; i++){
+        var variable = vetor[i]
+        let j = i - 1
+
+        while(j >= 0 && vetor[j] > variable ){
+            vetor[j + 1] = vetor[j]
+            j--
         }
+        vetor[j + 1] = variable       
     }
 }
 
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        const key = arr[i];
-        let j = i - 1;
-        while (j >= 0 && key < arr[j]) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
+    let tamanho = 1000
+    let minimo = 1
+    let maximo = 1000
+    const vetorRandom = []
+    const vetorOrganizado = []
+    const vetorInvertido = []
+
+    for(let i = 0; i < tamanho; i++){
+        vetorRandom[i] = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
+        vetorOrganizado[i] = i + 1
+        vetorInvertido[i] = 1000 - i
+
     }
-}
-
-// Função para testar os algoritmos de ordenação
-
-function testSortingAlgorithms(arr) {
-    console.log("Tamanho do vetor:", arr.length);
-
-    // Clone do vetor original para usar em todos os algoritmos
-    const arrCopy = [...arr];
-
-    // Bubblesort
-    const startBubblesort = new Date().getTime();
-    bubblesort(arrCopy);
-    const endBubblesort = new Date().getTime();
-    console.log(`Bubblesort: ${(endBubblesort - startBubblesort) / 1000} segundos`);
-
-    // Seleção Direta
-    const arrCopySelection = [...arr];
-    const startSelection = new Date().getTime();
-    selectionSort(arrCopySelection);
-    const endSelection = new Date().getTime();
-    console.log(`Seleção Direta: ${(endSelection - startSelection) / 1000} segundos`);
-
-    // Inserção Direta
-    const arrCopyInsertion = [...arr];
-    const startInsertion = new Date().getTime();
-    insertionSort(arrCopyInsertion);
-    const endInsertion = new Date().getTime();
-    console.log(`Inserção Direta: ${(endInsertion - startInsertion) / 1000} segundos`);
-}
-
-// Tamanho do vetor
-const n = 1000;
-
-// Vetor aleatório
-const randomArr = Array.from({ length: n }, () => Math.floor(Math.random() * n) + 1);
-testSortingAlgorithms(randomArr);
-
-// Vetor ordenado
-const sortedArr = Array.from({ length: n }, (_, index) => index + 1);
-testSortingAlgorithms(sortedArr);
-
-// Vetor invertido
-const reversedArr = Array.from({ length: n }, (_, index) => n - index);
-testSortingAlgorithms(reversedArr);
+    console.log(vetorRandom)
+    console.log(vetorOrganizado)
+    console.log(vetorInvertido)
+    console.log("Tempo de Execução BubbleSort")
+    console.time('VetorRandom')
+    bubbleSort(vetorRandom)
+    console.timeEnd('VetorRandom')
+    console.time('VetorOrganizado')
+    bubbleSort(vetorOrganizado)
+    console.timeEnd('VetorOrganizado')
+    console.time('VetorInvertido')
+    bubbleSort(vetorInvertido)
+    console.timeEnd('VetorInvertido')
+    console.log('Tempo de Execução SelectionSort')
+    console.time('VetorRandom')
+    selectionSort(vetorRandom)
+    console.timeEnd('VetorRandom')
+    console.time('VetorOrganizado')
+    selectionSort(vetorOrganizado)
+    console.timeEnd('VetorOrganizado')
+    console.time('VetorInvertido')
+    selectionSort(vetorInvertido)
+    console.timeEnd('VetorInvertido')
+    console.log("Tempo de Execução InsertionSort")
+    console.time('VetorRandom')
+    insertionSort(vetorRandom)
+    console.timeEnd('VetorRandom')
+    console.time('VetorOrganizado')
+    insertionSort(vetorOrganizado)
+    console.timeEnd('VetorOrganizado')
+    console.time('VetorInvertido')
+    insertionSort(vetorInvertido)
+    console.timeEnd('VetorInvertido')
