4dc21ae7d1236d29078269a62f97ebfa25d3dc91
Cássio <cassiocr2012@hotmail.com>
2023-10-05 10:17:55 -0300

    modified:   Teste.js

--- Teste.js
+++ Teste.js
@@ -1,10 +1,30 @@
-var fila = []
-var x
-x = parseInt(prompt("Informe um número"))
-fila.push(x)
-fila.push(1)
-fila.push(2)
-alert("Fila: " + fila)
-x = fila.shift()
-alert("elemento: " + x)
-alert("Fila: " + fila)
+function pilha(){
+    var fila = []
+    var x
+
+    x = parseInt(prompt("Informe um número"))
+    fila.push(x)
+    fila.push(1)
+    fila.push(2)
+    alert("Fila: " + fila)
+    x = fila.shift()
+    alert("elemento: " + x)
+    alert("Fila: " + fila)
+}
+
+function fila(){
+    var pilha = []
+    var x
+    
+    x = parseInt(prompt("Informe um número"))
+    
+    pilha.push(x)
+    pilha.push(1)
+    pilha.push(2)
+    
+    alert("Pilha: " + pilha)
+    x = pilha.pop()
+    
+    alert("elemento: " + x)
+    alert("Pilha: " + pilha)
+}
